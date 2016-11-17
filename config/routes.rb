b9cba7f53af30c523b977fa64c86c55d3fb5ba03
nmccormick <nmccormick95@gmail.com>
2016-11-18 10:43:25 +1100

    added integration test, and added named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get '/help', to: 'static_pages#help'
-
-  get '/about' to: 'static_pages#about'
-
-  get '/contact' to: 'static_ages#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help' #creates two variables: help_path and help_url
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
