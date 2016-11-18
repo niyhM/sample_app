2661c480b119f582f20dfaa02378c2301439f875
nmccormick <nmccormick95@gmail.com>
2016-11-18 11:12:59 +1100

    Added a signup path, and named route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help' #creates two variables: help_path and help_url
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
