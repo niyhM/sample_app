1fdab2f8a2f1c22fcd49d7ce9546a5a3fa1b518a
nmccormick <nmccormick95@gmail.com>
2016-11-25 19:49:14 +1100

    Login stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	before_save { self.email.downcase! } #passes block to before_save callback
 	validates :name, presence:true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -9,10 +11,33 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    def User.digest(string)
+    def self.digest(string)
     	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
    		BCrypt::Password.create(string, cost: cost)
 	end
+
+	def self.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#remember method will assign a new URL to this users remember token field
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	#returns true if the given token matches the digest.
+	#grabs remember_digest (from User db) and compares to the remember_token in the class
+	#which is grabbed from the http request.
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+    	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+    	update_attribute(:remember_digest, nil)
+    end
+
 end
 	
