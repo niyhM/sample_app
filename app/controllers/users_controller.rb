1f2f1a5b1ec36ae1f33e9248a36ebcf67692bde1
nmccormick <nmccormick95@gmail.com>
2016-11-28 00:14:16 +1100

    added index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 require 'pry'
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index 
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -29,11 +35,41 @@ class UsersController < ApplicationController
   	end
   end
 
-  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
+  def edit
+  end
 
   private
 
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+
+    #Before Filters
+
+    #confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,11 @@ module SessionsHelper
     end
   end
 
+  #returns true if given user is current user
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
@@ -47,4 +52,15 @@ module SessionsHelper
   	cookies.delete(:user_id)
   	cookies.delete(:remember_token)
   end
+
+  # Redirects to stored location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default) 
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
